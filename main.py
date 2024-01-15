ee9ba75c041b4e71987ae1d58feaacb642faa252
Giacomo Sfratato <xbit18@hotmail.it>
2024-01-15 20:28:19 +0100

    mss

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import cv2
 from PIL import ImageGrab
 import numpy as np
 import time
+import mss
 
 
-start = time.time()
-for i in range(20):
-    for j in range(10):
-        for t in range(7):
-            for r in range(8):
-                image1 = ImageGrab.grab(bbox=(40,40,100,100))
-                # image = cv2.imread("screenshot2.png")
-                # cv2.imshow("title", image)
-                # cv2.destroyAllWindows()
-end = time.time()
+time_pil = 0
+time_mss = 0
 
-print(end-start)
+for i in range(1000):
+    start = time.time()
+    with mss.mss() as sct:
+        monitor = {"top": 40, "left": 40, "width": 100, "height": 100}
+        img_array = np.array(sct.grab(monitor))
+    end = time.time()
+    time_mss += end-start
+
+    start = time.time()
+    image1 = ImageGrab.grab(bbox=(40,40,100,100))
+    end = time.time()
+
+    time_pil += end-start
+    # image = cv2.imread("screenshot2.png")
+    # cv2.imshow("title", image)
+    # cv2.destroyAllWindows()
+
+print(f"MSS: {time_mss}, PIL: {time_pil}")
 
 
