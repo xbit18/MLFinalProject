48a107b5944d1dcc7db2886b7f67ec9b52361091
Xbit18 <xbit18@hotmail.it>
2024-01-15 20:09:54 +0100

    test

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import time
 
 
 start = time.time()
-for i in range(1000):
-    image1 = ImageGrab.grab(bbox=(40,40,100,100))
-    # image = cv2.imread("screenshot2.png")
-    # cv2.imshow("title", image)
-    # cv2.destroyAllWindows()
+for i in range(20):
+    for j in range(10):
+        for t in range(7):
+            for r in range(8):
+                image1 = ImageGrab.grab(bbox=(40,40,100,100))
+                # image = cv2.imread("screenshot2.png")
+                # cv2.imshow("title", image)
+                # cv2.destroyAllWindows()
 end = time.time()
 
 print(end-start)
