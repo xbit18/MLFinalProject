f2cdfbecc25d85231859febfcc4323f36ea68f4c
Giacomo Sfratato <xbit18@hotmail.it>
2024-01-15 19:45:13 +0100

    test

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from PIL import ImageGrab
 import numpy as np
 import time
 
-# image = cv2.imread("screenshot2.png")
-# cv2.imshow("title", image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+
 
 start = time.time()
-image1 = ImageGrab.grab(bbox=(40,40,100,100))
+#image1 = ImageGrab.grab(bbox=(40,40,100,100))
+image = cv2.imread("screenshot2.png")
+cv2.imshow("title", image)
+cv2.destroyAllWindows()
 end = time.time()
 
 print(end-start)
