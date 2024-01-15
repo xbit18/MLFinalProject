e390ce77fed5961134818fdffef69d40d4981146
Giacomo Sfratato <xbit18@hotmail.it>
2024-01-15 20:03:57 +0100

    test

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import time
 time_sum = 0
 for i in range(1000):
     start = time.time()
-    #image1 = ImageGrab.grab(bbox=(40,40,100,100))
-    image = cv2.imread("screenshot2.png")
-    cv2.imshow("title", image)
-    cv2.destroyAllWindows()
+    image1 = ImageGrab.grab(bbox=(40,40,100,100))
+    # image = cv2.imread("screenshot2.png")
+    # cv2.imshow("title", image)
+    # cv2.destroyAllWindows()
     end = time.time()
     time_sum += end - start
 
