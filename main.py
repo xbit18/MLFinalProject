86ca322d245a42ffa1d32bebd32f65de05bef379
Xbit18 <xbit18@hotmail.it>
2024-01-26 20:18:51 +0100

    works

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ import mss
 import numpy as np
 import os
 import pandas as pd
+import pynput.keyboard
 import selenium.common.exceptions
-from pynput.keyboard import Controller, Key
+from pynput.keyboard import Controller
+import multiprocessing
+from concurrent.futures import *
+from pathlib import Path
+from selenium.webdriver import Keys
 
 from Tetromino import *
 from selenium import webdriver
@@ -138,7 +143,7 @@ def board_recognition(img):
 
             coords = int(block_y + block_height / 2), int(block_x + block_width / 2)
 
-            if img_grey[coords] >= 1:
+            if img_grey[coords] >= 30:
                 board_array[board_row, board_col] = 1
 
     return board_array
@@ -160,14 +165,14 @@ def check_full_line(piece, board_array):
     return False
 
 
-def convert_screen(board_array):
+def convert_screen(board_array, part, video):
     image_array = board_array.copy()
     for i in range(20):
         for j in range(10):
             if image_array[i, j] == 1:
                 image_array[i, j] = 255
 
-    path = f"boards/{get_time_string()}.png"
+    path = f"./boards/video{video}/{part}/{get_time_string()}.png"
     return image_array, path
 
 
@@ -183,24 +188,30 @@ def check_game_ended(board_array):
             return True
 
 
-def check_game_started(score_coords):
+def check_game_started(score_coords, score_template_path):
     with mss.mss() as sct:
         monitor = tuple(score_coords)
         score = np.array(sct.grab(monitor))[:, :, :3]
 
-        scale_percent = 50  # percent of original size
-        width = int(score.shape[1] * scale_percent / 100)
-        height = int(score.shape[0] * scale_percent / 100)
-        dim = (width, height)
 
-        score = cv2.resize(score, dim, interpolation=cv2.INTER_AREA)
+        # scale_percent = 50  # percent of original size
+        # width = int(score.shape[1] * scale_percent / 100)
+        # height = int(score.shape[0] * scale_percent / 100)
+        # dim = (width, height)
+        #
+        # score = cv2.resize(score, dim, interpolation=cv2.INTER_AREA)
 
-    template = cv2.imread('./images/score_template.png', cv2.IMREAD_GRAYSCALE)
+    template = cv2.imread(score_template_path, cv2.IMREAD_GRAYSCALE)
+    w, h = template.shape[::-1]
 
-    method = cv2.TM_CCOEFF_NORMED
+    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+               'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+
+    method = eval(methods[1])
     img_gray = cv2.cvtColor(score, cv2.COLOR_RGB2GRAY)
     res = cv2.matchTemplate(img_gray, template, method)
-    threshold = 0.95
+    threshold = 0.9
     loc = np.where(res >= threshold)
     lst = sorted(zip(*loc[::-1]))
 
@@ -208,145 +219,239 @@ def check_game_started(score_coords):
     return found
 
 
-def check_video_ended():
-    pass
+def main(board_coords, score_coords, time_to_end, part, video, score_template_path):
+    try:
 
+        start_time = time.time()
+        last_piece = (None, None)
+        images = []
+        all_images = []
+        previous_image = np.zeros((20, 10))
+        board_array = np.zeros((20, 10))
+        previous_board = np.zeros((20, 10))
 
-def main(board_coords, score_coords):
-    last_piece = (None, None)
-    images = []
-    all_images = []
-    previous_image = np.zeros((20, 10))
-    board_array = np.zeros((20, 10))
+        game_started = False
+        to_print = ""
+        while True:
+            if time.time() > start_time + time_to_end:
+                break
 
-    game_started = False
-    while True:
-        cls()
-        if check_video_ended():
-            break
+            if not game_started:  # Se la partita non è cominciata, controlla di nuovo
+                to_print = "Game has not started"
+                game_started = check_game_started(score_coords, score_template_path)
 
-        if not game_started:  # Se la partita non è cominciata, controlla di nuovo
-            print(f"Game has not started")
-            game_started = check_game_started(score_coords)
+            else:  # Se è cominciata
 
-        else:  # Se è cominciata
+                with mss.mss() as sct:
+                    monitor = tuple(board_coords)
+                    image1 = np.array(sct.grab(monitor))
 
-            with mss.mss() as sct:
-                monitor = tuple(board_coords)
-                image1 = np.array(sct.grab(monitor))
+                board_array = board_recognition(image1)
 
-            board_array = board_recognition(image1)
-
-            if check_game_ended(board_array):  # Se la partita è finita, aspetta di nuovo che cominci
-                print(f"Game has ended")
-                game_started = False
-                continue
-
-            print(f"Game has started")
-
-            # Se la board è vuota
-            if np.array_equal(board_array, np.zeros((20, 10))):
-                previous_board = board_array
-                continue
-
-            current_piece, full_line = find_piece(board_array)
-
-            # Se non ho trovato un pezzo ora, e non ne avevo trovato prima, continuo a cercare
-            if current_piece == (None, None) and last_piece == (None, None):
-                previous_board = board_array
-                continue
-
-            # Il pezzo corrente è nella sua posizione finale, devo:
-            # - cercare tale posizione
-            # - salvarla
-            # - settare last piece a (None, None) per dire che va cercato un nuovo pezzo
-            if current_piece == (None, None) and last_piece != (None, None):
-
-                final_piece = check_last_piece(last_piece, board_array, previous_board)
-                if final_piece is None:
+                if check_game_ended(board_array):  # Se la partita è finita, aspetta di nuovo che cominci
+                    to_print = "Game has ended"
+                    game_started = False
                     continue
 
-                for i in range(len(images)):
-                    images[i]["type"] = final_piece[0]
-                    images[i]["rotation"] = final_piece[1]
-                    images[i]["final_col"] = final_piece[2]
+                # Se la board è vuota
+                if np.array_equal(board_array, np.zeros((20, 10))):
+                    previous_board = board_array
+                    continue
 
-                all_images.extend(images)
-                images = []
-                last_piece = (None, None)
+                current_piece, full_line = find_piece(board_array)
+                if full_line:
+                    time.sleep(0.7)
 
-                previous_board = board_array
+                # Se non ho trovato un pezzo ora, e non ne avevo trovato prima, continuo a cercare
+                if current_piece == (None, None) and last_piece == (None, None):
+                    previous_board = board_array
+                    continue
 
-            # Devo:
-            # - aggiornare la posizione del pezzo corrente
-            # - salvare la board nell'array images
-            if current_piece != (None, None):  # and last_piece != (None, None):
-                image_array, path = convert_screen(board_array)
-                if not np.array_equal(image_array, previous_image):
-                    images.append({"path": path, "image": image_array})
-                    previous_image = image_array
-                last_piece = current_piece
+                # Il pezzo corrente è nella sua posizione finale, devo:
+                # - cercare tale posizione
+                # - salvarla
+                # - settare last piece a (None, None) per dire che va cercato un nuovo pezzo
+                if current_piece == (None, None) and last_piece != (None, None):
 
-    for im in all_images:
-        cv2.imwrite(im['path'], im['image'])
-        del im['image']
+                    final_piece = check_last_piece(last_piece, board_array, previous_board)
+                    if final_piece is None:
+                        continue
+                    to_print = final_piece
 
-    df = pd.DataFrame(all_images, columns=["path", "type", "rotation", "final_col"])
-    df.to_csv(f"dataset_{get_time_string()}")
+                    for i in range(len(images)):
+                        images[i]["type"] = final_piece[0]
+                        images[i]["rotation"] = final_piece[1]
+                        images[i]["final_col"] = final_piece[2]
 
+                    all_images.extend(images)
+                    images = []
+                    last_piece = (None, None)
+
+                    previous_board = board_array
+
+                # Devo:
+                # - aggiornare la posizione del pezzo corrente
+                # - salvare la board nell'array images
+                if current_piece != (None, None):  # and last_piece != (None, None):
+                    to_print = current_piece
+                    image_array, path = convert_screen(board_array, part, video)
+                    if not np.array_equal(image_array, previous_image):
+                        images.append({"path": path, "image": image_array})
+                        previous_image = image_array
+                    last_piece = current_piece
+
+            cls()
+            print(to_print)
+            time.sleep(0.1)
+        
+        for im in all_images:
+            cv2.imwrite(im['path'], im['image'])
+            del im['image']
+
+        df = pd.DataFrame(all_images, columns=["path", "type", "rotation", "final_col"])
+        df.to_csv(f"./datasets/dataset_{video}_{part}.csv")
+    except KeyboardInterrupt:
+
+        for im in all_images:
+            cv2.imwrite(im['path'], im['image'])
+            del im['image']
+
+        df = pd.DataFrame(all_images, columns=["path", "type", "rotation", "final_col"])
+        df.to_csv(f"./datasets/dataset_{video}_{part}.csv")
+
+def parse_duration(duration):
+    elems = str(duration).split(":")
+    if len(elems) == 3:
+        hrs = int(elems[0])
+        mins = int(elems[1])
+        secs = int(elems[2])
+    else:
+        hrs = 0
+        mins = int(elems[0])
+        secs = int(elems[1])
+
+    time_to_pass = hrs * 3600 + mins * 60 + secs
+
+    return time_to_pass
 
 if __name__ == '__main__':
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
-    from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 
+
+    # Creiamo un'istanza di Firefox, installiamo un adblocker per saltare le pubblicità
     options = webdriver.FirefoxOptions()
     driver = webdriver.Firefox(options=options)
     driver.install_addon("./ublockorigin.xpi")
+
+    keyboard = Controller()
     time.sleep(1)
 
     videos = [
         {
-            "url": "https://www.youtube.com/watch?v=QC8iQqtG0hg&pp=ygUPNSBzZWNvbmRzIHZpZGVv", #https://www.youtube.com/watch?v=nfo8hmIcoDQ",
-            "board_coords": [[465, 297, 610, 589], [653, 297, 798, 589]],
-            "score_coords": [[440, 150, 632, 260], [632, 150, 824, 260]]
-        }
+            "url": "https://www.youtube.com/watch?v=nfo8hmIcoDQ",
+            "board_coords": [[713, 347, 928, 780], [992, 347, 1207, 780]],
+            "score_coords": [[690, 145, 950, 263], [970, 145, 1230, 263]],
+            "score_template": "./images/score_template0.png",
+            "duration": 2220
+        },
+        {
+            "url": "https://www.youtube.com/watch?v=bcAGhChRu6k",
+            "board_coords": [[709, 301, 952, 789], [968, 301, 1210, 789]],
+            "score_coords": [[740, 130, 960, 220], [960, 130, 1175, 220]],
+            "score_template": "./images/score_template1.png",
+            "duration": 3780
+        },
     ]
 
+    # accepted_cookies = False
+    # maximized = False
+    #for video in videos:
+    #     driver.get(video['url'])
+    #     driver.add_cookie({"name": "wide", "value": "1"})
+    #
+    #     if not maximized:
+    #         driver.maximize_window()
+    #         maximized = True
+    #
+    #     if not accepted_cookies:
+    #         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,
+    #                                                                     "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button"))).click()
+    #         accepted_cookies = True
+    #
+    #     input()
+    #
+    #     with mss.mss() as sct:
+    #         leftboard = tuple(video['board_coords'][0])
+    #         rightboard = tuple(video['board_coords'][1])
+    #         boardL = np.array(sct.grab(leftboard))
+    #         boardR = np.array(sct.grab(rightboard))
+    #
+    #         leftscore = tuple(video['score_coords'][0])
+    #         rightscore = tuple(video['score_coords'][1])
+    #         scoreL = np.array(sct.grab(leftscore))
+    #         scoreR = np.array(sct.grab(rightscore))
+    #
+    #     cv2.imshow("Boards", np.concatenate((boardL, boardR), axis=1))
+    #     cv2.imshow("Scores", np.concatenate((scoreL, scoreR), axis=1))
+    #     cv2.waitKey(0)
+    #     cv2.destroyAllWindows()
+    #
+    #     input_str = input()
+    #     if input_str == 'y':
+    #         continue
+    #     else:
+    #         print(f"Video {video['id']} has wrong coordinates")
+    #         driver.quit()
+    #         quit()
+
+
+
     accepted_cookies = False
-    cinema_button_pressed = False
-    for video in videos:
+    maximized = False
+
+    # Per ogni video,
+    # - apriamo la pagina
+    # - settiamo la modalità cinema
+    # - in un loop infinito, controlliamo lo stato del video
+
+    for i in range(len(videos)):
+        video = videos[i]
+        Path(f"./boards/video{i}/left").mkdir(parents=True, exist_ok=True)
+        Path(f"./boards/video{i}/right").mkdir(parents=True, exist_ok=True)
+
         driver.get(video['url'])
         driver.add_cookie({"name": "wide", "value": "1"})
-        driver.maximize_window()
+
+        if not maximized:
+            driver.maximize_window()
+            maximized = True
 
         if not accepted_cookies:
             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button"))).click()
             accepted_cookies = True
 
-        # driver.refresh()
+        time.sleep(5)
         while True:
-            button = driver.find_element(By.CSS_SELECTOR, "button.ytp-play-button")
-            print(button.get_attribute("aria-label"))
-        # time.sleep(7)
-        # keyboard = Controller()
-        # keyboard.press(Key.space)
-        # keyboard.release(Key.space)
-        #
-        # # cinema_button = "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div[2]/div/div/ytd-player/div/div/div[30]/div[2]/div[2]/button[7]"
-        # # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, cinema_button))).click()
-        #
-        # while True:
-        #     try:
-        #         btn = driver.find_element(By.XPATH, "/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div[2]/ytd-player/div/div/div[36]/div[2]/div[1]/button")
-        #         print(btn.get_attribute("title"))
-        #         if btn.get_attribute("title") == "Riproduci (k)":
-        #             btn.click()
-        #         elif btn.get_attribute("title") == "Rivedi":
-        #             break
-        #     except:
-        #         pass
-        # cls()
-        # print("video ended")
-        # # main(video['board_coords'], video['score_coords'])
+            try:
+                player_status = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
+                break
+            except selenium.common.exceptions.JavascriptException:
+                continue
+
+        # Se il video non è iniziato, premi K per avviarlo
+        if player_status == -1: #video has not started playing
+            keyboard.press("k")
+            keyboard.release("k")
+
+        with ProcessPoolExecutor(2) as executor:
+            futures = []
+            left = executor.submit(main, video['board_coords'][0], video['score_coords'][0], video['duration'], 'left', i, video['score_template'])
+            right = executor.submit(main, video['board_coords'][1], video['score_coords'][1], video['duration'], 'right', i, video['score_template'])
+
+            # print('Waiting for tasks to complete...')
+            wait(futures, return_when=ALL_COMPLETED)
+
+    driver.quit()
+    quit()
